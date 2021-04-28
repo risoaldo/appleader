baf1ad698e0788e68ddf7fddc6445481837b930f
Risoaldo Nóbrega <noobregarisoaldo@gmail.com>
2021-04-27 22:34:56 -0300

    initial commit more login page

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../pages/SignIn';
+import Main from '../pages/Main';
+
+const Auth = createStackNavigator();
+
+
+
+const AuthRoutes = () => {
+  return (
+    <Auth.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#312e38' },
+      }}
+    >
+      <Auth.Screen name="SignIn" component={SignIn} />
+      <Auth.Screen name="Main" component={Main} />
+    </Auth.Navigator>
+  )
+}
+
+export default AuthRoutes;
